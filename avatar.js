b1e3398755b069fc05103c4689515ed57c0cb5ed
Aleksandar Damjanović <49364900+aleksaDam997@users.noreply.github.com>
2025-11-15 12:23:44 +0100

    Fix filename case for avatar model loa

--- avatar.js
+++ avatar.js
@@ -6,7 +6,7 @@ window.onload = () => loadModel();
 
 function loadModel() {
   const loader = new GLTFLoader();
-  loader.load('avatar.glb',
+  loader.load('Avatar.glb',
     (gltf) => {
       setupScene(gltf);
       document.getElementById('avatar-loading').style.display = 'none';
